c0e3991f03c748d39ced68a452b98b9fd4b7ca70
Pavani <kadaripavani1@gmail.com>
2025-01-20 22:45:25 +0530

    Fixes #37: Formatting code done in backend and gitignore updated with uploads and payment_images

--- finance-hive/backend/models/User.js
+++ finance-hive/backend/models/User.js
@@ -1,19 +1,3 @@
-// // backend/models/User.js
-// const mongoose = require('mongoose');
-
-// const userSchema = new mongoose.Schema({
-//   role: { type: String, required: true },
-//   email: { type: String, required: true, unique: true },
-//   firstName: { type: String, required: true },
-//   lastName: { type: String, required: true },
-//   userId: { type: String, required: true },
-//   mobileNumber: { type: String, required: true },
-//   address: { type: String, required: true },
-//   password: { type: String, required: true },
-// });
-
-// const User = mongoose.model('User', userSchema);
-// module.exports = User;
 
 const mongoose = require('mongoose');
 
