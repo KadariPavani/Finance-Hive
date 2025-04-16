5b02bcd7848374c61d8298bee0eacdce27bd52d5
Pavani <kadaripavani1@gmail.com>
2025-04-16 21:45:59 +0530

    updated frontend url

--- finance-hive/backend/server.js
+++ finance-hive/backend/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "https://finance-hive.netlify.app/", // Replace with your frontend URL
+  origin: "https://finance-hive.netlify.app", // Replace with your frontend URL
   credentials: true
 }));
 app.use(bodyParser.json());
