66bcd51463a18a15bc5a293ae32b9590279d4bfe
Pavani <kadaripavani1@gmail.com>
2025-06-07 12:41:03 +0530

    Fix image

--- finance-hive/frontend/src/components/home/TeamGrid.js
+++ finance-hive/frontend/src/components/home/TeamGrid.js
@@ -1,20 +1,51 @@
 import React, { useState } from "react";
-import '../home/TeamGrid.css';
+import "../home/TeamGrid.css";
 const TeamGrid = () => {
   const [currentMemberIndex, setCurrentMemberIndex] = useState(0);
 
   const teamMembers = [
-    { name: "Namala Monika", content: "Team Lead", imgSrc: "/Images/Monika.jpg" },
-    { name: "Pavani Kadari", content: "Junior Developer", imgSrc: "/Images/pannu.jpg" },
-    { name: "Yogitha Yadla", content: "Senior Developer", imgSrc: "/Images/yogitha.jpg" },
-    { name: "Jothsna Pandranki", content: "Junior Developer", imgSrc: "Images/joo.jpg" },
-    { name: "Pavani Borusu", content: "Senior Developer", imgSrc: "/Images/Pavani.jpg" },
+    {
+      name: "Namala Monika",
+      content: "Team Lead",
+      imgSrc: "/Images/Monika.jpg",
+    },
+    {
+      name: "Pavani Kadari",
+      content: "Junior Developer",
+      imgSrc: "/Images/pannu.jpg",
+    },
+    {
+      name: "Yogitha Yadla",
+      content: "Senior Developer",
+      imgSrc: "/Images/yogitha.jpg",
+    },
+    {
+      name: "Jothsna Pandranki",
+      content: "Junior Developer",
+      imgSrc: "Images/joo.jpg",
+    },
+    {
+      name: "Pavani Borusu",
+      content: "Senior Developer",
+      imgSrc: "/Images/Pavani.jpg",
+    },
     { name: "Ramya", content: "Senior Developer", imgSrc: "Images/Ramya.jpg" },
-    { name: "Subrahmanyam Penujuli", content: "Senior Developer", imgSrc: "/Images/Subrahmanyam.jpg" },
-    { name: "Sai Varshitha", content: "Junior Developer", imgSrc: "/Images/varshi.jpg" },
-    { name: "Priyanka Mullu", content: "Junior Developer", imgSrc: "/Images/Priyanka.jpg" },
+    {
+      name: "Subrahmanyam Penujuli",
+      content: "Senior Developer",
+      imgSrc: "/Images/Subrahmanyam.jpg",
+    },
+    {
+      name: "Sai Varshitha",
+      content: "Junior Developer",
+      imgSrc: "/Images/Varshi.jpg",
+    },
+    {
+      name: "Priyanka Mullu",
+      content: "Junior Developer",
+      imgSrc: "/Images/Priyanka.jpg",
+    },
   ];
-  
 
   const handleMouseOver = (index) => {
     setCurrentMemberIndex(index);
